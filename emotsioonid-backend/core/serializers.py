b3611a64f2a55dfc3250610f31be3bcfd8ad4ad1
ivartammela-stack <ivar.tammel@vikk.ee>
2025-10-25 00:18:42 +0300

    push

--- emotsioonid-backend/core/serializers.py
+++ emotsioonid-backend/core/serializers.py
@@ -1,9 +1,13 @@
+# core/serializers.py
 from rest_framework import serializers
 from .models import School, Student
+
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = ['id', 'name', 'created_at']
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'name', 'school', 'created_at']
-
-
