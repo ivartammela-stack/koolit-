d2947704476efeae975cf8a2d25231b4b6d506cf
ivartammela-stack <ivar.tammel@vikk.ee>
2025-10-25 11:15:19 +0300

    push

--- emotsioonid-backend/core/models.py
+++ emotsioonid-backend/core/models.py
@@ -4,6 +4,8 @@ class School(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 class Student(models.Model):
     name = models.CharField(max_length=255)
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name="students")
